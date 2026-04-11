5da08d12113ec40318e2dee9c1845e1737b2106e
Sven De Waerhert <sven@waerhert.dev>
2026-04-11 12:11:53 +0200

    refactor(core): lowercase machine and middleware filenames

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,4 @@
 export * from './types.js';
-export * from './Machine.js';
-export * from './Middleware.js';
+export * from './machine.js';
+export * from './middleware.js';
 export * from './utils.js';
